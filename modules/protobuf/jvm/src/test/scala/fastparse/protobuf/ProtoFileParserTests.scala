3441bcf395216d242cafc0ba44ccf06927af7b2a
James Collier <james.collier@disneystreaming.com>
2019-08-26 00:54:34 +0100

    more testing examples

--- modules/protobuf/jvm/src/test/scala/fastparse/protobuf/ProtoFileParserTests.scala
+++ modules/protobuf/jvm/src/test/scala/fastparse/protobuf/ProtoFileParserTests.scala
@@ -8,11 +8,25 @@ object ProtoFileParserTests extends TestSuite {
   private def readResource(path: String) =
     scala.io.Source.fromResource(path).getLines.mkString("\n")
 
+  private def runParseSuccessChecks(path: String): Unit = {
+    val body = readResource(path)
+    val result = parse(body, ProtoFileParser.proto(_))
+    assert(result.isSuccess)
+  }
+
   override val tests = Tests {
     test("parse addressbook.proto example") - {
       val body = readResource("addressbook.proto")
       val result = parse(body, ProtoFileParser.proto(_))
       assert(result == Parsed.Success(expAddressbookProto, 1319))
+    }
+    test("parse wrappers_test.proto example") - {
+      val body = readResource("wrappers_test.proto")
+      val result = parse(body, ProtoFileParser.proto(_))
+      assert(result == Parsed.Success(expwrappersTestProto, 2477))
+    }
+    test("successfully parse examples") - {
+      test("proto3_message.proto") - runParseSuccessChecks("proto3_message.proto")
     }
   }
 
@@ -43,4 +57,22 @@ object ProtoFileParserTests extends TestSuite {
     ))
   ))
 
+  private lazy val expwrappersTestProto = ProtoFile(List(
+    Package("wrappers_test"),
+    Import(None, "google/protobuf/wrappers.proto"),
+    OptionExpr("java_package", StringConstant("com.google.protobuf.wrapperstest")),
+    OptionExpr("java_outer_classname", StringConstant("WrappersTestProto")),
+    Message("TopLevelMessage", List(
+      Field(repeated = false, "int32", "field1" ,1,List.empty),
+      Field(repeated = false,".google.protobuf.DoubleValue", "field_double", 2, List.empty),
+      Field(repeated = false, ".google.protobuf.FloatValue", "field_float", 3, List.empty),
+      Field(repeated = false, ".google.protobuf.Int64Value", "field_int64", 4, List.empty),
+      Field(repeated = false, ".google.protobuf.UInt64Value", "field_uint64",5 ,List.empty),
+      Field(repeated = false, ".google.protobuf.Int32Value", "field_int32", 6, List.empty),
+      Field(repeated = false, ".google.protobuf.UInt32Value", "field_uint32", 7, List.empty),
+      Field(repeated = false, ".google.protobuf.BoolValue", "field_bool", 8, List.empty),
+      Field(repeated = false, ".google.protobuf.StringValue", "field_string", 9, List.empty),
+      Field(repeated = false, ".google.protobuf.BytesValue", "field_bytes", 10, List.empty)
+    ))
+  ))
 }
